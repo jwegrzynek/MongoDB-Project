0d560476f6d43199790e0a70818b29092f8366bf
jwegrzynek <kuba.lysy.weg@gmail.com>
2025-06-10 07:23:11 +0200

    added new tests to main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from bson import ObjectId
 
 if __name__ == "__main__":
-    uri = ("mongodb+srv://dbNonRelProject:project123@projectcluster.u5uvzky.mongodb.net/"
-           "?retryWrites=true&w=majority&appName=ProjectCluster")
-    # uri = "mongodb://localhost:27017"
+    #uri = ("mongodb+srv://dbNonRelProject:project123@projectcluster.u5uvzky.mongodb.net/"
+           #"?retryWrites=true&w=majority&appName=ProjectCluster")
+    uri = "mongodb://localhost:27017"
     pet_db = PetAdoptionDatabase(uri=uri)
 
     # print("\n Create pet document:")
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # }
     # pet_db.create_pet(sample_pet)
 
-    # pet_db.read_pets({'_id': 11111})
+    # pet_db.read_pets({'_id': 14991})
 
     # pet_db.delete_pet({'_id': 11111})
     # pet_db.read_pets({'_id': 11111})
@@ -48,4 +48,6 @@ if __name__ == "__main__":
 
     # pet_db.get_recent_rescues()
     # pet_db.get_pets_by_shelter_stay(stay_type="longest", n=3, threshold_months=12, comparison= 'shorter')
-    pet_db.is_ready_for_adoption(14990)
+    # pet_db.is_ready_for_adoption(14993)
+    pet_db.adopt_pet(14990)
+
